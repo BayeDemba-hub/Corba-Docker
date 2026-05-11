aa5faf2c3cf87da2fc5f154ed2aa18b2d36d2880
BayeDemba-hub <dembandiaye696@gmail.com>
2026-05-11 18:33:50 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-echo "Démarrage ORBD..."
+python -m http.server 10000 &
 
 orbd -ORBInitialPort 900 &
+
 sleep 5
 
-echo "Démarrage serveur CORBA..."
-
 java \
-  -Dorg.omg.CORBA.ORBInitialHost=localhost \
-  -Dorg.omg.CORBA.ORBInitialPort=900 \
-  -classpath /app/corba-pdf-server.jar \
-  com.corba.pdf.PDFServer \
-  -ORBInitialHost localhost \
-  -ORBInitialPort 900
+-Dorg.omg.CORBA.ORBInitialHost=localhost \
+-Dorg.omg.CORBA.ORBInitialPort=900 \
+-classpath /app/corba-pdf-server.jar \
+com.corba.pdf.PDFServer \
+-ORBInitialHost localhost \
+-ORBInitialPort 900
